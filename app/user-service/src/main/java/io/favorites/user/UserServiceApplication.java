970bffd69201d4b6b12fbe5edb33762d54df44aa
Liu Mingyuan <mingyuan.liu@tieto.com>
2019-09-18 14:36:41 +0800

    init user-service project and refactor dependencies

---
+++ app/user-service/src/main/java/io/favorites/user/UserServiceApplication.java
@@ -0,0 +1,16 @@
+package io.favorites.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ app/user-service/src/main/java/io/favorites/user/services/UserRepository.java
@@ -0,0 +1,62 @@
+package io.favorites.user.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import io.favorites.user.domain.User;
+import org.springframework.transaction.annotation.Transactional;
+
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("from User u where u.userName=:userName")
+    User findByUserName(@Param("userName") String userName);
+
+    @Query("from User u where u.userName=:userName and u.email=:email")
+    User findByUserNameOrEmail(@Param("userName") String userName, @Param("email") String email);
+
+    @Query("from User u where u.email=:email")
+    User findByEmail(@Param("email") String email);
+
+    @Query("from User u where u.id=:id")
+    User findById(@Param("id") long  id);
+
+    @Modifying(clearAutomatically=true)
+    @Transactional
+    @Query("update User set outDate=:outDate, validataCode=:validataCode where email=:email")
+    int setOutDateAndValidataCode(@Param("outDate") String outDate, @Param("validataCode") String validataCode, @Param("email") String email);
+
+    @Modifying(clearAutomatically=true)
+    @Transactional
+    @Query("update User set passWord=:passWord where email=:email")
+    int setNewPassword(@Param("passWord") String passWord, @Param("email") String email);
+
+    @Modifying(clearAutomatically=true)
+    @Transactional
+    @Query("update User set introduction=:introduction where email=:email")
+    int setIntroduction(@Param("introduction") String introduction, @Param("email") String email);
+
+    @Modifying(clearAutomatically=true)
+    @Transactional
+    @Query("update User set userName=:userName where email=:email")
+    int setUserName(@Param("userName") String userName, @Param("email") String email);
+
+    @Modifying(clearAutomatically=true)
+    @Transactional
+    @Query("update User set profilePicture=:profilePicture where id=:id")
+    int setProfilePicture(@Param("profilePicture") String profilePicture, @Param("id") Long id);
+
+    @Modifying(clearAutomatically=true)
+    @Transactional
+    @Query("update User set backgroundPicture=:backgroundPicture where id=:id")
+    int setBackgroundPicture(@Param("backgroundPicture") String backgroundPicture, @Param("id") Long id);
+
+}
