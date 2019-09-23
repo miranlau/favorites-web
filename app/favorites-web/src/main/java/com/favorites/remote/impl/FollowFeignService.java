1a3de8cf602fd350e0111ff98584246f7a3c05c9
Song Xing <xing.song@tieto.com>
2019-09-23 20:02:35 +0800

    fix bug

--- app/favorites-web/src/main/java/com/favorites/remote/impl/FollowFeignService.java
+++ app/favorites-web/src/main/java/com/favorites/remote/impl/FollowFeignService.java
@@ -20,30 +20,30 @@ public interface FollowFeignService {
 	@RequestMapping(value = "/follows", method = { RequestMethod.POST })
 	void save(@RequestBody Follow follow);
 	
-	@RequestMapping(value = "/search/findByUserId")
+	@RequestMapping(value = "/follows/search/findByUserId")
 	ListResult<User> findByUserId(@RequestParam("userId") Long userId);
 	
-	@RequestMapping(value = "/search/findMyFollowIdByUserId")
+	@RequestMapping(value = "/follows/search/findMyFollowIdByUserId")
 	ListResult<Follow> findMyFollowIdByUserId(@RequestParam("userId") Long userId);
 	
-	@RequestMapping(value = "/search/countByUserIdAndStatus")
+	@RequestMapping(value = "/follows/search/countByUserIdAndStatus")
 	Integer countByUserIdAndStatus(@RequestParam("userId") Long userId, @RequestParam("status") FollowStatus status);
 	
-	@RequestMapping(value = "/search/countByFollowIdAndStatus")
+	@RequestMapping(value = "/follows/search/countByFollowIdAndStatus")
 	Integer countByFollowIdAndStatus(@RequestParam("followId") Long followId, @RequestParam("status") FollowStatus status);
 	
-	@RequestMapping(value = "/search/findFollowUserByUserId")
+	@RequestMapping(value = "/follows/search/findFollowUserByUserId")
 	ListResult<User> findFollowUserByUserId(@RequestParam("userId") Long userId);
 	
-	@RequestMapping(value = "/search/findFollowedUserByFollowId")
+	@RequestMapping(value = "/follows/search/findFollowedUserByFollowId")
 	ListResult<User> findFollowedUserByFollowId(@RequestParam("followId") Long followId);
 	
-	@RequestMapping(value = "/search/countByUserIdAndFollowIdAndStatus")
+	@RequestMapping(value = "/follows/search/countByUserIdAndFollowIdAndStatus")
 	Integer countByUserIdAndFollowIdAndStatus(@RequestParam("userId") Long userId, @RequestParam("followId") Long followId, @RequestParam("status") FollowStatus status);
 	
-	@RequestMapping(value = "/search/findByUserIdAndFollowId")
+	@RequestMapping(value = "/follows/search/findByUserIdAndFollowId")
 	Follow findByUserIdAndFollowId(@RequestParam("userId") Long userId, @RequestParam("followId") Long followId);
 	
-	@RequestMapping(value = "/search/updateStatusById")
+	@RequestMapping(value = "/follows/search/updateStatusById")
 	Integer updateStatusById(@RequestParam("status") FollowStatus status, @RequestParam("lastModifyTime") Long lastModifyTime, @RequestParam("id") Long id);
 }
