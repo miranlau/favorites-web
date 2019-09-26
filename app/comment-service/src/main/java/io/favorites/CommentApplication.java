259a2aa00ba52640fe1953159a1303050824e128
yyyvvc <fish_yyy@163.com>
2019-09-26 17:00:08 +0800

    remove eureka & correct extends from favorites-parent project

--- app/comment-service/src/main/java/io/favorites/CommentApplication.java
+++ app/comment-service/src/main/java/io/favorites/CommentApplication.java
@@ -2,10 +2,8 @@ package io.favorites;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
 public class CommentApplication {
 
 	public static void main(String[] args) {
