4eeda2f11bc409dc9386d025403f615b06bb2b88
yyyvvc <fish_yyy@163.com>
2019-09-26 16:44:46 +0800

    remove eureka & correct extends from favorites-parent project

--- app/notice-service/src/main/java/io/favorites/NoticeApplication.java
+++ app/notice-service/src/main/java/io/favorites/NoticeApplication.java
@@ -2,11 +2,8 @@ package io.favorites;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class NoticeApplication {
 
     public static void main(String[] args) {
