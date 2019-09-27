b78f070e45e8dc28d3bab47aabb8beeca436d2cd
Tang Nan <nan.tang@tieto.com>
2019-09-27 19:13:14 +0800

    update countByUserIdAndTypeAndReaded sql

--- app/notice-service/src/main/java/io/favorites/repository/NoticeRepository.java
+++ app/notice-service/src/main/java/io/favorites/repository/NoticeRepository.java
@@ -15,18 +15,10 @@ import javax.transaction.Transactional;
 @RepositoryRestResource(path = "notices", collectionResourceRel = "data")
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
-	public String baseSql="select c.id as id,c.title as title, c.type as type,c.url as url," +
-			"c.favoritesId as favoritesId,c.remark as favoriteName,c.logoUrl as logoUrl,c.userId as userId, "
-			+ "c.remark as remark,c.description as description,c.lastModifyTime as lastModifyTime, "
-			+ "u.userName as userName,u.profilePicture as profilePicture,n.comment.id as operId "
-			+ "from Notice n,Collect c,User u WHERE n.collect.id=c.id and c.userId=u.id";
-	
-	@Query(baseSql+ " and n.user.id=?1 and n.type=?2")
-	Page<CollectView> findViewByUserIdAndType(@Param(value = "userId") Long userId, @Param(value = "type")String type, Pageable pageable);
-
+	@Query("select count(1) from Notice n where n.user.id=?1 and n.type=?2 and n.readed=?3")
 	Long countByUserIdAndTypeAndReaded(@Param(value = "userId")Long userId, @Param(value = "type")String type, @Param(value = "readed")String readed);
 
-	@Query("select count(1) from Notice n,Praise p where n.comment.id=p.id and type='praise' and n.user.id=?1 and n.readed=?2")
+	@Query("select count(1) from Notice n,Praise p where n.comment.id=p.id and n.type='praise' and n.user.id=?1 and n.readed=?2")
 	Long countPraiseByUserIdAndReaded(@Param(value = "userId")Long userId, @Param(value = "readed")String readed);
 	
 	@Transactional
