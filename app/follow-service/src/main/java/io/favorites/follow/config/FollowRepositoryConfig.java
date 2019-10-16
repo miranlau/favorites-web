7698b5338de8b0c5496c99121bd6d516c25c7963
hanqiankun <hanqiankun@gmail.com>
2019-10-16 15:35:48 +0800

    output Id for User which will be used in url link

--- app/follow-service/src/main/java/io/favorites/follow/config/FollowRepositoryConfig.java
+++ app/follow-service/src/main/java/io/favorites/follow/config/FollowRepositoryConfig.java
@@ -5,12 +5,13 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
 import io.favorites.follow.domain.Follow;
+import io.favorites.follow.domain.User;
 
 @Configuration
 public class FollowRepositoryConfig extends RepositoryRestConfigurerAdapter {
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        config.exposeIdsFor(Follow.class);
+        config.exposeIdsFor(Follow.class).exposeIdsFor(User.class);
     }
 }
